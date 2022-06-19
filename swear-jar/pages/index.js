0b567e692fcb71d26962d67a58c362b8e2bd1949
Kirby McKenzie <kirby.mckenzie@hotmail.com>
2022-06-19 13:11:59 +1000

    Update to use chrome storage

--- swear-jar/pages/index.js
+++ swear-jar/pages/index.js
@@ -1,11 +1,16 @@
 import { useTheme } from "next-themes";
 import { SunIcon, MoonIcon } from "@heroicons/react/solid";
 import { useState, useEffect } from "react";
+import { useChromeStorageLocal } from "use-chrome-storage";
 
 const IndexPage = () => {
   const [mounted, setMounted] = useState(false);
-  const [balance, setBalance] = useState(0);
   const { systemTheme, theme, setTheme } = useTheme();
+
+  const [balance, setBalance, isPersistent, error] = useChromeStorageLocal(
+    "counterLocal",
+    0
+  );
 
   useEffect(() => {
     setMounted(true);
@@ -35,34 +40,29 @@ const IndexPage = () => {
 
   if (!mounted) return null;
 
-  // Create our number formatter.
   const formatter = new Intl.NumberFormat("en-US", {
     style: "currency",
     currency: "USD",
-
-    // These options are needed to round to whole numbers if that's what you want.
-    //minimumFractionDigits: 0, // (this suffices for whole numbers, but will print 2500.10 as $2,500.1)
-    //maximumFractionDigits: 0, // (causes 2500.99 to be printed as $2,501)
   });
 
-  /* $2,500.00 */
-
   return (
-    <main className="max-w-lg mx-auto p-4">
+    <main className="max-w-lg mx-auto p-4" style={{ minWidth: 400 }}>
       <div className=" white container flex justify-between items-center">
-        <h1 className="text-3xl font-bold text-center">{"Swear Jar"}</h1>
+        <h1 className="text-3xl font-bold text-center">
+          {`Balance: ${formatter.format(balance)}`}
+        </h1>
         {renderThemeChanger()}
       </div>
 
-      <div className="flex flex-col">
-        <div className="my-2">
-          <span>{`Balance: ${formatter.format(balance)}`}</span>
-        </div>
+      <div className="flex flex-col mt-4">
         <button
           className="bg-red-600 dark:bg-red-500 hover:bg-yellow-800 text-white font-medium py-4 px-4 rounded border-b-8 border-red-700 active:border-b-0 active:translate-y-1"
-          onClick={() => setBalance(balance + 1)}
+          onClick={() => setBalance(balance + 2)}
         >
           {"Deposit $2.00 "}
+        </button>
+        <button className="mt-4" onClick={() => setBalance(0)}>
+          {"Clear"}
         </button>
       </div>
     </main>
