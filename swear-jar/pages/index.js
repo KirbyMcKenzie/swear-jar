9c009706d9dd66a56b4509158640c4c2c13c261f
Kirby McKenzie <kirby.mckenzie@hotmail.com>
2022-06-19 12:12:03 +1000

    Add darkmode

--- swear-jar/pages/index.js
+++ swear-jar/pages/index.js
@@ -1,12 +1,50 @@
-import styles from "../styles/Home.module.css";
+import { useTheme } from "next-themes";
+import { SunIcon, MoonIcon } from "@heroicons/react/solid";
+import { useState, useEffect } from "react";
 
 const IndexPage = () => {
+  const [mounted, setMounted] = useState(false);
+  const { systemTheme, theme, setTheme } = useTheme();
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  const renderThemeChanger = () => {
+    const currentTheme = theme === "system" ? systemTheme : theme;
+
+    if (currentTheme === "dark") {
+      return (
+        <SunIcon
+          className="w-10 h-10 text-slate-500"
+          role="button"
+          onClick={() => setTheme("light")}
+        />
+      );
+    } else {
+      return (
+        <MoonIcon
+          className="w-10 h-10 text-slate-500"
+          role="button"
+          onClick={() => setTheme("dark")}
+        />
+      );
+    }
+  };
+
+  if (!mounted) return null;
+
   return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      </main>
-    </div>
+    <main className="max-w-lg mx-auto">
+      <header className="h-15m dark:border-gray-700">
+        <div className=" white dark:bg-slate-800 container px-4 sm:px-6 py-4 flex justify-between items-center">
+          <h1 className="text-3xl text-slate-500 font-bold text-center">
+            {" Hello world!"}
+          </h1>
+          {renderThemeChanger()}
+        </div>
+      </header>
+    </main>
   );
 };
 
