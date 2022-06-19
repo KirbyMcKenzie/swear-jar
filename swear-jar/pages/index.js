ab4b9d680e1d0398d7419723edf587deb65093ca
Kirby McKenzie <kirby.mckenzie@hotmail.com>
2022-06-19 16:14:26 +1000

    Add additional styling and swear count

--- swear-jar/pages/index.js
+++ swear-jar/pages/index.js
@@ -2,14 +2,29 @@ import { useTheme } from "next-themes";
 import { SunIcon, MoonIcon } from "@heroicons/react/solid";
 import { useState, useEffect } from "react";
 import { useChromeStorageLocal } from "use-chrome-storage";
+import { useReward } from "react-rewards";
+
+const isDevelopment = process.env.NODE_ENV === "development";
 
 const IndexPage = () => {
   const [mounted, setMounted] = useState(false);
   const { systemTheme, theme, setTheme } = useTheme();
 
+  const { reward: confettiReward, isAnimating: isConfettiAnimating } =
+    useReward("confettiReward", "emoji", {
+      emoji: ["🤬", "👿", "🤑", "😬"],
+      position: "absolute",
+      angle: "90",
+    });
+
   const [balance, setBalance] =
     process.env.NODE_ENV === "production"
-      ? useChromeStorageLocal("counterLocal", 0)
+      ? useChromeStorageLocal("balanceLocal", 0.0)
+      : useState(0);
+
+  const [numSwears, setNumSwears] =
+    process.env.NODE_ENV === "production"
+      ? useChromeStorageLocal("numSwearsLocal", 0.0)
       : useState(0);
 
   useEffect(() => {
@@ -22,7 +37,7 @@ const IndexPage = () => {
     if (currentTheme === "dark") {
       return (
         <SunIcon
-          className="w-8 h-8 text-white"
+          className="w-6 h-6 text-slate-300"
           role="button"
           onClick={() => setTheme("light")}
         />
@@ -30,7 +45,7 @@ const IndexPage = () => {
     } else {
       return (
         <MoonIcon
-          className="w-8 h-8 text-black"
+          className="w-6 h-6 text-slate-700"
           role="button"
           onClick={() => setTheme("dark")}
         />
@@ -46,23 +61,44 @@ const IndexPage = () => {
   });
 
   return (
-    <main className="max-w-lg mx-auto p-4" style={{ minWidth: 400 }}>
+    <main
+      className="max-w-lg mx-auto p-4"
+      style={{ minWidth: 400, maxHeight: 800, overflow: "hidden" }}
+    >
       <div className="container flex justify-between items-center">
-        <h1 className="text-3xl font-bold text-center">
-          {`Balance: ${formatter.format(balance)}`}
-        </h1>
+        <div>
+          <h1 className="text-2xl font-semibold text-center text-slate-700 dark:text-slate-300 mr-2">
+            {`Balance: ${formatter.format(balance)}`}
+          </h1>
+        </div>
         {renderThemeChanger()}
       </div>
 
+      <p className="text-sm text-slate-500">{`Kindly donated from ${numSwears} swears.`}</p>
+
       <div className="flex flex-col mt-4">
+        <div className="flex flex-col" style={{ minHeight: 66 }}>
+          <button
+            id="confettiReward"
+            className="bg-red-600 dark:bg-red-500 text-white font-medium py-4 px-4 rounded border-b-8 border-red-700 active:border-b-0 active:translate-y-1"
+            onClick={() => {
+              setBalance(balance + 2.0);
+              setNumSwears(numSwears + 1);
+              confettiReward();
+            }}
+          >
+            {"Oh, #$%!"}
+          </button>
+        </div>
+        <p></p>
         <button
-          className="bg-red-600 dark:bg-red-500 hover:bg-yellow-800 text-white font-medium py-4 px-4 rounded border-b-8 border-red-700 active:border-b-0 active:translate-y-1"
-          onClick={() => setBalance(balance + 2)}
+          className="mt-2 p-2 rounded-md text-slate-500 text-sm hover:bg-slate-200 hover:dark:bg-slate-800 hover:dark:text-slate-500"
+          onClick={() => {
+            setBalance(0);
+            setNumSwears(0);
+          }}
         >
-          {"Deposit $2.00 "}
-        </button>
-        <button className="mt-4" onClick={() => setBalance(0)}>
-          {"Clear"}
+          {"Reset Balance"}
         </button>
       </div>
     </main>
