fa94d6f8e5baae10e1b96a44966fe2ac1da5eaf6
Kirby McKenzie <kirby.mckenzie@hotmail.com>
2022-06-19 12:45:36 +1000

    Increment balance on button click

--- swear-jar/pages/index.js
+++ swear-jar/pages/index.js
@@ -4,6 +4,7 @@ import { useState, useEffect } from "react";
 
 const IndexPage = () => {
   const [mounted, setMounted] = useState(false);
+  const [balance, setBalance] = useState(0);
   const { systemTheme, theme, setTheme } = useTheme();
 
   useEffect(() => {
@@ -16,7 +17,7 @@ const IndexPage = () => {
     if (currentTheme === "dark") {
       return (
         <SunIcon
-          className="w-10 h-10 text-slate-500"
+          className="w-8 h-8 text-white"
           role="button"
           onClick={() => setTheme("light")}
         />
@@ -24,7 +25,7 @@ const IndexPage = () => {
     } else {
       return (
         <MoonIcon
-          className="w-10 h-10 text-slate-500"
+          className="w-8 h-8 text-black"
           role="button"
           onClick={() => setTheme("dark")}
         />
@@ -34,16 +35,36 @@ const IndexPage = () => {
 
   if (!mounted) return null;
 
+  // Create our number formatter.
+  const formatter = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+
+    // These options are needed to round to whole numbers if that's what you want.
+    //minimumFractionDigits: 0, // (this suffices for whole numbers, but will print 2500.10 as $2,500.1)
+    //maximumFractionDigits: 0, // (causes 2500.99 to be printed as $2,501)
+  });
+
+  /* $2,500.00 */
+
   return (
-    <main className="max-w-lg mx-auto">
-      <header className="h-15m dark:border-gray-700">
-        <div className=" white dark:bg-slate-800 container px-4 sm:px-6 py-4 flex justify-between items-center">
-          <h1 className="text-3xl text-slate-500 font-bold text-center">
-            {" Hello world!"}
-          </h1>
-          {renderThemeChanger()}
+    <main className="max-w-lg mx-auto p-4">
+      <div className=" white container flex justify-between items-center">
+        <h1 className="text-3xl font-bold text-center">{"Swear Jar"}</h1>
+        {renderThemeChanger()}
+      </div>
+
+      <div className="flex flex-col">
+        <div className="my-2">
+          <span>{`Balance: ${formatter.format(balance)}`}</span>
         </div>
-      </header>
+        <button
+          className="bg-red-600 dark:bg-red-500 hover:bg-yellow-800 text-white font-medium py-4 px-4 rounded border-b-8 border-red-700 active:border-b-0 active:translate-y-1"
+          onClick={() => setBalance(balance + 1)}
+        >
+          {"Deposit $2.00 "}
+        </button>
+      </div>
     </main>
   );
 };
